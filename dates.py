02229d13b9402cd0002905f8219089043a5c644f
João <joaoluisok@gmail.com>
2020-03-27 22:38:15 -0300

    dates and intervals

---
+++ dates.py
@@ -0,0 +1,34 @@
+from datetime import datetime as dt
+import pytz
+# from tzlocal import get_localzone
+
+def get_currentdate():
+    """ returns current date """
+    pass
+
+def iterate_interval():
+    """ iterate over given date interval in df"""
+    pass
+
+def parse_date():
+    """returns date in list format day/month/year/hour/min"""
+    pass
+
+def dfinterval():
+    """ returns a df such that the date is in the given interval"""
+    pass
+
+def days_back():
+    """ returns a df such that the date is from x days ago """
+
+def weeks_back():
+    """ returns a df such that the date is from x weeks ago """
+    pass
+
+def months_back():
+    """ returns a df such that the date is from x months ago """
+    pass
+
+def years_back():
+    """ returns a df such that the date is from x years ago """
+    pass
