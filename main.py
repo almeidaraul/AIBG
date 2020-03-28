b405d774ac477303bcd4d5a8951dda2b99f39dda
Raul Almeida <haltsimog@gmail.com>
2020-03-28 18:56:14 -0300

    Add datetime conversion to primitive preprocessing

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
+import datetime as dt
 from explorer import Explorer
 
-# pega csv e transforma em dataframe do pandas
+# get example data and put it into a pandas dataframe
 df = pd.read_csv('example_data.csv')
+
+# turn all dates into actual datetime objects:
+df.date = df.date.apply(lambda date_string : 
+        dt.datetime.strptime(date_string, '%Y-%m-%d %H:%M')
+
 ex = Explorer(df)
-print(ex.bg_avg())
