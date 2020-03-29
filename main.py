59df30233639cc1c748081092ed8d00a97a34040
Raul Almeida <haltsimog@gmail.com>
2020-03-29 11:26:38 -0300

    Apply pep8 to existing code

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import datetime as dt
 import numpy as np
 import pandas as pd
-import datetime as dt
 from explorer import Explorer
 
-# get example data and put it into a pandas dataframe
+# tests on example data
+
 df = pd.read_csv('example_data.csv')
 
 # turn all dates into actual datetime objects:
-df.date = df.date.apply(lambda date_string : 
-        dt.datetime.strptime(date_string, '%Y-%m-%d %H:%M'))
+df.date = df.date.apply(lambda date_string 
+    : dt.datetime.strptime(date_string, '%Y-%m-%d %H:%M'))
 
 ex = Explorer(df)
-print(ex.basic_stats('bg', 'avg', 'snack', 'before'), " +- ", ex.basic_stats('bg', 'std', 'snack', 'before'))
+print(ex.basic_stats('bg', 'avg', 'snack', 'before'), 
+    " +- ", ex.basic_stats('bg', 'std', 'snack', 'before'))
 print(ex.HbA1c())
 print(ex.range_time())
