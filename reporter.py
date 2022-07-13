c0000551e8e4b8769533331bd9ab686687284b28
Raul Almeida <almeidaraulgp@gmail.com>
2022-07-12 23:21:09 -0300

    add dict return to reporter, create jsonreporter

--- reporter.py
+++ reporter.py
@@ -1,70 +1,55 @@
 from analyser import Analyser
 from explorer import Explorer
-import matplotlib.pyplot as plt
-import numpy as np
+import json
 
 class Reporter():
     def __init__(self, explorer):
         self.explorer = explorer
         self.analyser = Analyser(self.explorer.df)
 
-    def plot_tir(self, in_range, below_range, above_range, fname):
-        """plot time in range piechart"""
-        values = np.array([in_range, below_range, above_range])
-        total = in_range + below_range + above_range
-        in_range = int(100*in_range/total)
-        below_range = int(100*below_range/total)
-        above_range = int(100*above_range/total)
-        labels = [f"In Range ({in_range}%)", f"Below Range({below_range}%)",
-                  f"Above Range({above_range}%)"]
-        plt.pie(values, labels=labels, startangle=90)
-        plt.title("Time in Range")
-        plt.savefig(fname)
-        plt.clf()
-
-    def plot_grouped_glucose(self, y, x, error, xlabel, ylabel, title,
-            fname, incline_xlabel=False):
-        """plot glucose entries per hour of the day (mean and std)"""
-        plt.xticks(x, rotation=(90 if incline_xlabel else 0))
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.errorbar(x, y, error, linestyle='-', marker='o', ecolor='green',
-                     capsize=2)
-        plt.title(title)
-        plt.savefig(fname)
-        plt.clf()
-
     def get_values(self):
         """calculate all necessary information"""
+        report = {}
         self.explorer.last_x_days(90)
         # hba1c
-        hba1c = self.analyser.hba1c() #TODO
+        hba1c = self.analyser.hba1c()
+        report["hba1c"] = float(hba1c)
         self.explorer.reset_df().last_x_days(15)
+        self.analyser = Analyser(self.explorer.df)
         # tir
         in_range, below_range, above_range = self.analyser.tir(70, 180)
         tir_graph_fname = 'tir.png'
-        self.plot_tir(in_range, below_range, above_range, 'tir.png') #TODO
+        report["in_range"] = float(in_range)
+        report["below_range"] = float(below_range)
+        report["above_range"] = float(above_range)
         # total entries
-        total_entries = self.analyser.count() #TODO
+        total_entries = self.analyser.count()
+        report["total_entries"] = int(total_entries)
         groupby_day = self.analyser.groupby_day()
-        # entries per day
-        entries_per_day = groupby_day["date"].count() #TODO
+        # mean entries per day
+        entries_per_day = groupby_day["date"].count().mean()
+        report["entries_per_day"] = float(entries_per_day)
         # fast insulin total, mean, std per day
         fast_per_day = groupby_day["fast_insulin"].sum()
-        mean_fast_per_day = fast_per_day.mean() #TODO
-        std_fast_per_day = fast_per_day.std() #TODO
+        mean_fast_per_day = fast_per_day.mean()
+        std_fast_per_day = fast_per_day.std()
+        report["mean_fast_per_day"] = float(mean_fast_per_day)
+        report["std_fast_per_day"] = float(std_fast_per_day)
         # glucose mean, std per hour
         groupby_hour = self.analyser.groupby_hour()
         glucose_per_hour = {
             "mean": groupby_hour["glucose"].mean(),
             "std": groupby_hour["glucose"].std(),
         }
-        self.plot_grouped_glucose(y=glucose_per_hour["mean"],
-                                  x=np.array(range(24)),
-                                  error=glucose_per_hour["std"],
-                                  xlabel="Hour", ylabel="Glucose (mg/dL)",
-                                  title="Glucose per Hour",
-                                  fname="glucose_hour.png") #TODO
+        report["mean_glucose_per_hour"] = {
+                "mean_glucose": [
+                    float(x) for x in glucose_per_hour["mean"].tolist()
+                ],
+                "hour": list(groupby_hour.groups.keys()),
+                "std_error": [
+                    float(x) for x in glucose_per_hour["std"].tolist()
+                ],
+                }
         # all entries table (pretty)
         show_columns = {
             'date': 'Date',
@@ -84,8 +69,28 @@ class Reporter():
         for col in numeric_columns:
             table[col] = table[col].fillna(0).apply(lambda x: int(x))
         table = table.rename(columns=show_columns)
+        table = {
+                "Date": table["Date"].astype(str).tolist(),
+                "Glucose": [float(x) for x in table["Glucose"].tolist()],
+                "Bolus": [int(x) for x in table["Bolus"].tolist()],
+                "Correction": [int(x) for x in table["Correction"].tolist()],
+                "Basal": [int(x) for x in table["Basal"].tolist()],
+                "Meal": table["Meal"].tolist(),
+                "Carbohydrates": [int(x) for x in table["Carbohydrates"].tolist()],
+                }
+        report["table"] = table
+        return report
+
+class JSONReporter(Reporter):
+    def __init__(self, explorer):
+        super().__init__(explorer)
+    
+    def save(self, filename="report.json"):
+        report = super().get_values()
+        with open(filename, 'w') as f:
+            json.dump(report, f)
 
 if __name__=="__main__":
-    a = Explorer('diaguard.csv', verbose=True)
-    b = Reporter(a)
-    b.get_values()
+    a = Explorer('diaguard.csv', verbose=False)
+    b = JSONReporter(a)
+    b.save()
